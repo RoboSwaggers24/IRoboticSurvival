845dd226e177ad6017b2b1c651eb05d1e9a1b9dd
SMooren <sietse.mooren.1992@gmail.com>
2016-05-25 10:01:39 +0200

    newest push

--- LejorEV3/src/Cruiser.java
+++ LejorEV3/src/Cruiser.java
@@ -1,5 +1,8 @@
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.port.MotorPort;
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3TouchSensor;
+import lejos.hardware.sensor.SensorMode;
 import lejos.robotics.RegulatedMotor;
 import lejos.hardware.Button;
 import lejos.hardware.lcd.*;
@@ -9,10 +12,12 @@ public class Cruiser extends Thread {
 	LFJfedor lfjf;
 	RegulatedMotor mB = new EV3LargeRegulatedMotor(MotorPort.B);
 	RegulatedMotor mC = new EV3LargeRegulatedMotor(MotorPort.C);
+	EV3TouchSensor touch = new EV3TouchSensor(SensorPort.S1);
 	float color;
 	float turn;
 	int cTurn;
 	int bTurn;
+	final int maxPower = 250;
 	float power = 100;
 	float threshold;
 	
@@ -24,7 +29,7 @@ public class Cruiser extends Thread {
 
 		LCD.clear();
 		LCD.drawString("Started Cruiser", 0, 2);
-
+		
 		
 		while (!Button.ESCAPE.isDown()) {
 			if(Button.UP.isDown()){
@@ -35,10 +40,19 @@ public class Cruiser extends Thread {
 				power = power - 10;
 				System.out.println("power is: " + power);
 			}
-			
+			//while(touch.getTouchMode() ==){
+				
+			//}
 			threshold = (lfjf.lvh.getBlack() + lfjf.lvh.getWhite())/2;
 			color = LFUtils.getAvgLightValue();
 			double turn = (2.5 * power) * ((threshold-color)/(lfjf.lvh.getWhite() - lfjf.lvh.getBlack()));
+			if(power < maxPower && Math.abs(threshold - color) <= (0.5 * threshold)){
+				power += 1;
+			}
+			else if(Math.abs(threshold - color) > (0.5 * threshold)){
+				System.out.println(power);
+				power = 100;
+			}
 			
 			cTurn = (int) (power - turn);
 			bTurn = (int) (power + turn);
